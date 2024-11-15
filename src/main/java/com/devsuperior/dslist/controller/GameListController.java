3724fd705f7b5761c70d49226adab4fde87d334a
Gabrielogui <gabrielcsro@gmail.com>
2024-11-15 10:33:45 -0300

    DOCKER + MOVIMENTAÇÃO DA LISTA
    - Docker: Não está rodando, por segurança de pasta, então usar H2.
    - Método Mov: Movimentação de um game na lista alterando sua posição
    - GameListRepository: UPDATE no banco de dados para atualizar a posição do game

--- src/main/java/com/devsuperior/dslist/controller/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDto;
 import com.devsuperior.dslist.dto.GameMinDto;
+import com.devsuperior.dslist.dto.ReplacementDto;
 import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
@@ -22,18 +25,26 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    // |=======| GAME SERVICE |=======|
     @Autowired
     private GameService gameService;
 
+    // |=======| MÉTODOS |=======|
+    // FIND ALL: PEGAR TODOS OS GAMES  
     @GetMapping
     public List<GameListDto> findAll(){
         return gameListService.findAll();
     }
 
+    // FIND BY LIST: ENCONTRAR O GAME POR ID
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDto> findByList(@PathVariable Long listId){
         return gameService.findByList(listId); 
     }
 
-
+    // MOVIMENANDO A LISTA
+    @PostMapping(value = "/{listId}/raplacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDto body){
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+    }
 }
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDto;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -17,9 +19,39 @@ public class GameListService {
     @Autowired
     GameListRepository gameListRepository;
 
+    // |=======| GAME REPOSITORY |=======|
+    @Autowired
+    GameRepository gameRepository;
+
+    // |=======| MÉTODOS |=======|
+    // FIND ALL:
     @Transactional(readOnly = true)
     public List<GameListDto> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDto(x)).toList();
+    }
+
+    // MOVE: - MOVIMENTAR OS GAMES NA LISTA:
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min, max;
+
+        if(sourceIndex < destinationIndex){
+            min = sourceIndex;
+            max = destinationIndex;
+        }else{
+            min = destinationIndex;
+            max = sourceIndex;
+        }
+
+        for(int i = min ; i <= max ; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
